ab2c9fc7d84873e57f4d2ebdadfb10eef04badc2
Zekarias Asaminew <asaminewzekarias@johndeere.com>
2025-05-28 06:40:17 -0500

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'https://127.0.0.1:3001';
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const ENDPOINTS = {
   LAST_PLAYED: `${API_BASE_URL}/api/spotify/current-track`,
-} as const; 
+} as const;
